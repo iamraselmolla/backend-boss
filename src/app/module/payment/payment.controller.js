03e5255ca26758744b5880b76bafc859ee3f78f4
iamraselmolla <raselmolla6336@gmail.com>
2025-02-17 21:30:10 +0600

    added single payment finding and deletion api

--- src/app/module/payment/payment.controller.js
+++ src/app/module/payment/payment.controller.js
@@ -1,16 +1,56 @@
+import mongoose from "mongoose";
 import catchAsyncFunction from "../../share/catchAsyncFunc.js";
 import PaymentServices from "./payment.services.js";
 import httpStatus from "http-status";
 
 const createPayment = catchAsyncFunction(async (req, res, next) => {
-  const result = await PaymentServices.createPayment(req.body);
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Payment created successfully",
-    data: result,
-  });
+  try {
+    const result = await PaymentServices.createPayment(req.body);
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Payment created successfully",
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
-const PaymentController = { createPayment };
+const getAsinglePayment = catchAsyncFunction(async (req, res) => {
+  try {
+    const result = await PaymentServices.getAsinglePayment(req.params.id);
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Payment fetched successfully",
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+const deleteAPayment = catchAsyncFunction(async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return sendResponse(res, {
+        statusCode: httpStatus.BAD_REQUEST,
+        success: false,
+        message: "Invalid payment id",
+      });
+    }
+    const result = await PaymentServices.deleteAPayment(req.params.id);
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Payment deleted successfully",
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+const PaymentController = { createPayment, getAsinglePayment, deleteAPayment };
 export default PaymentController;
--- src/app/module/payment/payment.services.js
+++ src/app/module/payment/payment.services.js
@@ -3,6 +3,13 @@ import Payment from "./payment.model.js";
 const createPayment = async (paymentData) => {
   return await Payment.create(paymentData);
 };
+const getAsinglePayment = async (id) => {
+  return await Payment.findById(id);
+};
 
-const PaymentServices = { createPayment };
+const deleteAPayment = async (id) => {
+  return await Payment.findByIdAndDelete(id);
+};
+
+const PaymentServices = { createPayment, getAsinglePayment, deleteAPayment };
 export default PaymentServices;
